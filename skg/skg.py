b18f3b705b713a83c56cc3a7718392e9502ff7bf
jzwerling <jzwerling@users.noreply.github.com>
2019-04-25 22:27:46 -0700

    Update skg.py
    doh! bad commit, not sure what happened there.

--- skg/skg.py
+++ skg/skg.py
@@ -22,12 +22,11 @@ def get_edge_for_two_nodes(nodes, search_field, indexes_to_search, depth=1, leve
                 for n in nodes:
                     get_edge_for_two_nodes([n, b['key']], search_field, indexes_to_search, depth-1, level+1)
 
-<<<<<<< HEAD
-get_edge_for_two_nodes(["bruce banner", "iron man"], "entities", ["scifi_posts", "scifi_comments"], 2)
-#get_edge_for_two_nodes(["darth vader", "luke skywalker"], "entities", ["scifi_posts"], 3)
-#get_edge_for_two_nodes(["marty mcfly", "time travel"], "body_text", ["scifi_posts", "scifi_comments"], 3)
-=======
+                    
 get_edge_for_two_nodes(["jean grey", "in love"], "body_text", ["scifi_posts", "scifi_comments"])
 #get_edge_for_two_nodes(["vader", "the force"], "body_text", ["scifi_posts", "scifi_comments"], 3)
 #get_edge_for_two_nodes(["marty mcfly", "time travel"], "body_text", ["scifi_posts", "scifi_comments"], 5)
->>>>>>> c3385356a1f063083db8bb9042a5f1d0e9d0df18
+
+get_edge_for_two_nodes(["bruce banner", "iron man"], "entities", ["scifi_posts", "scifi_comments"], 2)
+#get_edge_for_two_nodes(["darth vader", "luke skywalker"], "entities", ["scifi_posts"], 3)
+#get_edge_for_two_nodes(["marty mcfly", "time travel"], "body_text", ["scifi_posts", "scifi_comments"], 3)
